3504bd883114f8f5445aded7e5c0c93f7c26a760
Lelonio <21023829+Lelonio@users.noreply.github.com>
2025-12-20 12:29:21 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Aumenta la versione per forzare l'aggiornamento
 const CACHE_NAME = "leleflix-v7"; // Aggiornato a v7
-const IMAGE_CACHE_NAME = "leleflix-images-v3"; // Nuova cache specifica per le immagini
+const IMAGE_CACHE_NAME = "leleflix-images-v4"; // Nuova cache specifica per le immagini
 
 const urlsToCache = [
   "./",
