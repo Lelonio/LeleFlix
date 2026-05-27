f08914278b00d6d41e2b8b3e323a379099e7075d
Lelonio <21023829+Lelonio@users.noreply.github.com>
2026-05-27 13:30:52 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Aumenta la versione per forzare l'aggiornamento
-const CACHE_NAME = "leleflix-v16"; // Aggiornato a v7
-const IMAGE_CACHE_NAME = "leleflix-images-v6"; // Nuova cache specifica per le immagini
+const CACHE_NAME = "leleflix-v20"; // Aggiornato a v7
+const IMAGE_CACHE_NAME = "leleflix-images-v8"; // Nuova cache specifica per le immagini
 
 const urlsToCache = [
   "./",
